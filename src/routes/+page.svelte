1d6c9ba3cb4138726701fb6f2d69f725c3d88ecb
Dan Grebb <git@dgrebb.com>
2024-09-01 07:01:06 -0400

    build(app): add tauri configuration and source files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="dashboard">
+	<h1>Dashboard</h1>
+</section>

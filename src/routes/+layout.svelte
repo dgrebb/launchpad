9a48707ee8eb64f1d5f82b4842d331a46630f3c6
Dan Grebb <git@dgrebb.com>
2024-09-01 08:05:31 -0400

    build(app): add shadcn components, dark/light mode toggle, basic layout, and app icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
-<script>
-	import '../app.css';
-	import '$lib/styles/global.pcss';
+<script lang="ts">
+	import '$lib/styles/app.css';
 	import type { Snippet } from 'svelte';
+	import { ModeWatcher, toggleMode, mode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import Icon from '@iconify/svelte';
 
 	interface Props {
 		children: Snippet;
@@ -10,6 +12,18 @@
 	let { children }: Props = $props();
 </script>
 
+<header>
+	<h1>LaunchPad</h1>
+	<ModeWatcher />
+	<Button on:click={toggleMode} variant="outline" size="icon">
+		<Icon
+			icon={$mode === 'light' ? 'mingcute:light-fill' : 'mingcute:light-line'}
+			height={33}
+			width={33}
+		/>
+		<span class="sr-only">Toggle theme</span>
+	</Button>
+</header>
 <main>
 	{#if children}
 		{@render children()}

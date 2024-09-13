364e29a055478219e634fa23d4a1fb063e9f70e7
Dan Grebb <git@dgrebb.com>
2024-09-13 05:14:51 -0400

    wip(svelte): comment web-only functionality

--- src/GLOBALS.js
+++ src/GLOBALS.js
@@ -1,8 +1,8 @@
-import { PUBLIC_JIRA_API_URL, PUBLIC_JIRA_API_PATH } from '$env/static/public';
+// import { PUBLIC_JIRA_API_URL, PUBLIC_JIRA_API_PATH } from '$env/static/public';
 
-export const JIRA = {
-	API: {
-		issuesByProject: `${PUBLIC_JIRA_API_URL}${PUBLIC_JIRA_API_PATH}/search?jql=project=`,
-		userProjects: `${PUBLIC_JIRA_API_URL}${PUBLIC_JIRA_API_PATH}/search?jql=project=`
-	}
-};
+// export const JIRA = {
+// 	API: {
+// 		issuesByProject: `${PUBLIC_JIRA_API_URL}${PUBLIC_JIRA_API_PATH}/search?jql=project=`,
+// 		userProjects: `${PUBLIC_JIRA_API_URL}${PUBLIC_JIRA_API_PATH}/search?jql=project=`
+// 	}
+// };
--- src/lib/api/API.jira.ts
+++ src/lib/api/API.jira.ts
@@ -1,63 +1,63 @@
-import { PUBLIC_JIRA_API_KEY, PUBLIC_JIRA_API_URL, PUBLIC_JIRA_API_USER } from '$env/static/public';
+// import { PUBLIC_JIRA_API_KEY, PUBLIC_JIRA_API_URL, PUBLIC_JIRA_API_USER } from '$env/static/public';
 
-const headers = new Headers();
-headers.append('content-type', 'application/json');
+// const headers = new Headers();
+// headers.append('content-type', 'application/json');
 
-export async function createRelease(name, description, project) {
-	const options = {
-		method: 'POST',
-		headers
-	};
-	const body = JSON.stringify({
-		archived: false,
-		description,
-		name,
-		project,
-		released: false
-	});
+// export async function createRelease(name, description, project) {
+// 	const options = {
+// 		method: 'POST',
+// 		headers
+// 	};
+// 	const body = JSON.stringify({
+// 		archived: false,
+// 		description,
+// 		name,
+// 		project,
+// 		released: false
+// 	});
 
-	try {
-		// Make the API POST call
-		const response = await fetch(PUBLIC_JIRA_API_URL, {
-			...options,
-			body
-		});
+// 	try {
+// 		// Make the API POST call
+// 		const response = await fetch(PUBLIC_JIRA_API_URL, {
+// 			...options,
+// 			body
+// 		});
 
-		if (response.ok) {
-			return 'success'; // Return 'success' if the API call succeeds
-		} else {
-			throw new Error('API call failed');
-		}
-	} catch (error) {
-		console.error('Error:', error);
-		throw error; // Rethrow the error
-	}
-}
+// 		if (response.ok) {
+// 			return 'success'; // Return 'success' if the API call succeeds
+// 		} else {
+// 			throw new Error('API call failed');
+// 		}
+// 	} catch (error) {
+// 		console.error('Error:', error);
+// 		throw error; // Rethrow the error
+// 	}
+// }
 
-export async function getUserProjects() {
-	const authString = `${PUBLIC_JIRA_API_KEY}:${PUBLIC_JIRA_API_USER}`;
-	const encodedAuth = btoa(authString);
+// export async function getUserProjects() {
+// 	const authString = `${PUBLIC_JIRA_API_KEY}:${PUBLIC_JIRA_API_USER}`;
+// 	const encodedAuth = btoa(authString);
 
-	headers.append('Authorization', `Basic ${encodedAuth}`);
-	const options = {
-		method: 'GET',
-		headers
-	};
+// 	headers.append('Authorization', `Basic ${encodedAuth}`);
+// 	const options = {
+// 		method: 'GET',
+// 		headers
+// 	};
 
-	try {
-		// Make the API POST call
-		const response = await fetch(PUBLIC_JIRA_API_URL, {
-			...options
-		});
+// 	try {
+// 		// Make the API POST call
+// 		const response = await fetch(PUBLIC_JIRA_API_URL, {
+// 			...options
+// 		});
 
-		if (response.ok) {
-			console.log('🚀 ~ getUserProjects ~ response:', response);
-			return response.json(); // Return 'success' if the API call succeeds
-		} else {
-			throw new Error('API call failed');
-		}
-	} catch (error) {
-		console.error('Error:', error);
-		throw error; // Rethrow the error
-	}
-}
+// 		if (response.ok) {
+// 			console.log('🚀 ~ getUserProjects ~ response:', response);
+// 			return response.json(); // Return 'success' if the API call succeeds
+// 		} else {
+// 			throw new Error('API call failed');
+// 		}
+// 	} catch (error) {
+// 		console.error('Error:', error);
+// 		throw error; // Rethrow the error
+// 	}
+// }

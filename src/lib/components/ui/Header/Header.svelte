0d97f9d08170c02623a52033a45789fb3401de84
Dan Grebb <git@dgrebb.com>
2024-09-13 05:17:36 -0400

    chore(svelte): install shadcn components (#8)

---
+++ src/lib/components/ui/Header/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { ModeWatcher, toggleMode, mode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button';
+	import Icon from '@iconify/svelte';
+	import GlobalNav from '../GlobalNav/GlobalNav.svelte';
+</script>
+
+<header>
+	<h1>LaunchPad</h1>
+	<ModeWatcher />
+	<Button on:click={toggleMode} variant="outline" size="icon">
+		<Icon
+			icon={$mode === 'light' ? 'mingcute:light-fill' : 'mingcute:light-line'}
+			height={33}
+			width={33}
+		/>
+		<span class="sr-only">Toggle theme</span>
+	</Button>
+	<GlobalNav />
+</header>
+
+<style>
+	/* your styles go here */
+</style>

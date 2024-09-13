23d0b511255d2aee105b2f21fb6a9257a8ef8edf
Dan Grebb <git@dgrebb.com>
2024-09-13 05:18:36 -0400

    feat(svelte): jira projects route, component, and mockdata loaded into state (#7)

---
+++ src/lib/components/Jira/Projects/JiraProjects.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Projects } from '@types';
+
+	interface Props {
+		projects: Projects;
+	}
+
+	const { projects }: Props = $props();
+</script>
+
+<h1>Jira Projects</h1>
+{#if projects.length > 0}
+	{#each projects as project}
+		<h2>{project.name}</h2>
+		<h3><code class="">{project.key}</code></h3>
+	{/each}
+{/if}
+
+<style>
+	/* your styles go here */
+</style>
---
+++ src/routes/jira/projects/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	// import { getUserProjects } from '@components/Jira/utils/API.jira';
+	import { projectsState } from '@state';
+	import mockProjects from '$lib/../test/mocks/jira/user-projects.json';
+	import JiraProjects from '@components/Jira/Projects/JiraProjects.svelte';
+
+	let projects = $state(projectsState.getProjects());
+
+	$effect(() => {
+		projectsState.setProjects(mockProjects);
+	});
+
+	$effect(() => {
+		projects = projectsState.getProjects();
+	});
+</script>
+
+<section class="dashboard">
+	<JiraProjects {projects} />
+</section>

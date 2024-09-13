cffbc9ffd52cae0b79cf749dd9d45b1dd6969d34
Dan Grebb <git@dgrebb.com>
2024-09-13 05:17:36 -0400

    chore(svelte): install shadcn components (#8)

---
+++ src/lib/components/ui/Footer/Footer.svelte
@@ -0,0 +1,5 @@
+<script>
+	let year = new Date().getFullYear();
+</script>
+
+<footer>LaunchPad | {year}</footer>
---
+++ src/lib/components/ui/GlobalNav/GlobalNav.svelte
@@ -0,0 +1,12 @@
+<script>
+	// your script goes here
+</script>
+
+<nav>
+	<a href="/jira">Jira</a>
+	<a href="/jira/projects">Jira Projects</a>
+</nav>
+
+<style>
+	/* your styles go here */
+</style>
